41d730386bc9358572ae864df51e08eebd30fafb
abhinavkkrsingh <abhinavkkrsingh@gmail.com>
2025-10-06 15:39:00 +0530

    done

--- routes/user_route.js
+++ routes/user_route.js
@@ -182,7 +182,51 @@ router.post("/forgot-password", async (req, res) => {
     return res.status(500).json({ message: "Server Error: " + err.message });
   }
 });
+// Route: Logged-in user can change password
+router.post("/change-password", CheckAuth, async (req, res) => {
+  try {
+    const { newPassword } = req.body;
 
+    // Validate input
+    if (!newPassword) {
+      return res.status(400).json({ message: "New password is required!" });
+    }
+
+    if (newPassword.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "New password must be at least 6 characters long!" });
+    }
+
+    const user = await blogUser.findById(req.user.userId, req.user.email);
+    console.log("userId", req.user.userId);
+    console.log("useremail", req.user.email);
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    if (user.isDeleted) {
+      return res.status(403).json({
+        message:
+          "Your account is marked for deletion and cannot reset password. It will be permanently deleted after 10 days.",
+      });
+    }
+
+    // Hash new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update password
+    user.password = hashedPassword;
+    await user.save();
+
+    return res.status(200).json({ message: "Password updated successfully!" });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+});
 // ------------------- Update Password -------------------
 // http://localhost:3000/user/update-password
 
